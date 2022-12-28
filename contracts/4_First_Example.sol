158ab6c4adc7a943d550cb7bb70abe619ec68740
mrtlive <mrtlive1@gmail.com>
2022-12-28 17:58:57 +0300

    first contract added

--- contracts/4_First_Example.sol
+++ contracts/4_First_Example.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.17;
 contract Counter {
     uint public count;
 
-    // Function to get the current count
+    //get current value
     function get() public view returns (uint) {
         return count;
     }
 
-    // Function to increment count by 1
+    // increment function for value add +1
     function inc() public {
         count += 1;
     }
 
-    // Function to decrement count by 1
+    // decrement function for value add -1
     function dec() public {
         // This function will fail if count = 0
         count -= 1;
